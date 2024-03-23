26af09a3960e85795bd89efa2b241c07edb34833
sinatra27 <147604618+sinatra27@users.noreply.github.com>
2024-03-23 04:00:39 -0400

    Add backspace button and update logic

--- script.js
+++ script.js
@@ -2,35 +2,31 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
-let calculated = false;
 
-// Select calculator display, operands, and operator
+// Select calculator buttons
+const display = document.querySelector('#display');
 const operands = document.querySelectorAll('.operand');
 const operators = document.querySelectorAll('.operator');
-const display = document.querySelector('#display');
 const equals = document.querySelector('#equal');
 const clear = document.querySelector('#clear');
 const plusMinus = document.querySelector('#sign');
 const percentage = document.querySelector('#percentage');
 const decimal = document.querySelector('#decimal');
-// const backspace = document.querySelector('#backspace');
+const backspace = document.querySelector('#backspace');
 
-// Update calculator operands, operator, and display when button click event is triggered
+// Update calculator operands, operator, and the display when button click event is triggered
 operands.forEach((num) => {
     num.addEventListener('click', () => {
         // event.stopPropagation();
         if (num1 === undefined) return;
-        if (calculated === true) {
-            num1 = 0;
-            calculated = false;
-        };
         if (operator === undefined) {
             num1 += num.value;
-            num1 = Number(num1);
+            // num1 = num1.toString() + num.value;      // can't type zero as decimal
+            num1 = +num1;
             display.innerHTML = num1;
         } else {
             num2 += num.value;
-            num2 = Number(num2);
+            num2 = +num2;
             display.innerHTML = num2;
         }
     });
@@ -41,17 +37,23 @@ operators.forEach((ops) => {
         // event.stopPropagation();
         // event.target.classList.toggle('selected');          // need to remove once calculated or cleared
         if (num1 === undefined) return;
-        operator = ops.value;
-        calculated = false;
+        if (operator === undefined) {
+            operator = ops.value;
+        } else {
+            operate();
+            operator = ops.value;
+        }        
     });
 });
 
-equals.addEventListener('click', operate);
+equals.addEventListener('click', () => {
+    operate();
+    operator = undefined;
+});
 
 clear.addEventListener('click', () => {
     num1 = num2 = 0;
     operator = undefined;
-    calculated = false;
     display.innerHTML = num1;
 });
 
@@ -77,20 +79,42 @@ percentage.addEventListener('click', () => {
     }
 });
 
+decimal.addEventListener('click', () => {
+    if (operator === undefined) {
+        if (String(num1).includes('.')) return;
+        num1 += '.';
+        display.innerHTML = num1;
+    } else {
+        if (String(num2).includes('.')) return;
+        num2 += '.';
+        display.innerHTML = num2;
+    }
+});
+
+backspace.addEventListener('click', () => {
+    if (num1 === undefined) return;
+    if (operator === undefined) {
+        // num1 = Math.floor(num1 / 10);
+        num1 = +(num1.toString().slice(0, -1));
+        display.innerHTML = num1;
+    } else {
+        num2 = +(num2.toString().slice(0, -1));
+        display.innerHTML = num2;
+    }
+});
+
 // Perform calculations by calling math functions
 function operate() {
+            console.log(num1, typeof(num1), operator, num2, typeof(num2));
     // Conditionals when no operator selected and when dividing by zero
     if (operator === undefined) return;
     if (num2 == 0 && operator == '/') num1 = undefined;
-    else if (operator == '+') num1 = add(num1, num2);
-    else if (operator == '-') num1 = subtract(num1, num2);
-    else if (operator == '*') num1 = multiply(num1, num2);
-    else num1 = divide(num1, num2);
+    else if (operator == '+') num1 = +(add(num1, num2)).toFixed(12);
+    else if (operator == '-') num1 = +(subtract(num1, num2)).toFixed(12);
+    else if (operator == '*') num1 = +(multiply(num1, num2)).toFixed(12);
+    else num1 = +(divide(num1, num2)).toFixed(12);
     display.innerHTML = num1;
-    calculated = true;
-    // Reset values
     num2 = 0;
-    operator = undefined;
 }
 
 // Addition function that accepts two numbers and returns the result
@@ -112,5 +136,3 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
     return n1 / n2;
 }
-
-// console.log(operate(num1, num2, operator));
